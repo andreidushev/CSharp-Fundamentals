27aeffaf8bcda2ea93eec51f6a1605cd5fe9ac05
Andrei Dushev <andrei4o6@abv.bg>
2020-03-06 18:31:08 +0200

    add

---
+++ Associative Arrays/A Miner Task/A Miner Task.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, int>();
+
+            while (true)
+            {
+                var resource = Console.ReadLine();
+
+                if (resource == "stop")
+                {
+                    break;
+                }
+                var quantity = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(resource))
+                {
+                    dict[resource] = 0;
+                }
+                dict[resource] += quantity;
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
