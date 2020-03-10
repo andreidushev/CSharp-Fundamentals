758f8568dd21e417b9713fe6470f75f2c872ac75
Andrei Dushev <andrei4o6@abv.bg>
2020-03-10 13:30:25 +0200

    add

---
+++ Associative Arrays/Courses/Courses.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Courses
+{
+    class Courses
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] arg = input.Split(" : ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string course = arg[0];
+                string student = arg[1];
+
+                if (!courses.ContainsKey(course))
+                {
+                    courses.Add(course,new List<string>());
+                    courses[course].Add(student);
+                }
+                else
+                {
+                    courses[course].Add(student);
+                }
+                
+            }
+            foreach (var course in courses.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count()}");
+                foreach (var name in course.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {name}");
+                }
+            }
+        }
+    }
+}
---
+++ Associative Arrays/SoftUni parking/SoftUni parking.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni_parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> usernamePlate = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                string comand = input[0];
+                string name = input[1];
+
+                if (comand == "register")
+                {
+                    string plate = input[2];
+
+                    if (!usernamePlate.ContainsKey(name))
+                    {
+                        usernamePlate.Add(name, plate);
+                        Console.WriteLine($"{name} registered {plate} successfully");
+                    }
+                    else
+                    {
+                        string currentPlate = plate;
+                        Console.WriteLine($"ERROR: already registered with plate number {currentPlate}");
+                    }
+                }
+                else if (comand == "unregister")
+                {
+                    if (usernamePlate.ContainsKey(name))
+                    {
+                        usernamePlate.Remove(name);
+                        Console.WriteLine($"{name} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {name} not found");
+                    }
+                }
+            }
+            foreach (var item in usernamePlate)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+    }
+}
